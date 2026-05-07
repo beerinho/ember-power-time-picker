23c7b9fbf693feecb5e96bfd3e8331a681be71b0
Daniel Beer <38084194+beerinho@users.noreply.github.com>
2026-05-07 08:23:37 +0200

    remove deprecated registerWaiter from ember-raf-scheduler

--- packages/test-app/tests/test-helper.js
+++ packages/test-app/tests/test-helper.js
@@ -4,11 +4,9 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
-import registerWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 
 setApplication(Application.create(config.APP));
 
-registerWaiter();
 setup(QUnit.assert);
 
 start();
