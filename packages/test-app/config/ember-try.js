dce13bc5817c5ef3ec9f3a188a883b4b534df75a
Daniel Beer <38084194+beerinho@users.noreply.github.com>
2026-07-15 12:52:09 +0200

    update testing matrix

--- packages/test-app/config/ember-try.js
+++ packages/test-app/config/ember-try.js
@@ -45,14 +45,6 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-lts-5.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~5.8.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-5.12',
         npm: {
           devDependencies: {
